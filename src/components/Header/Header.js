3db504e8ae382c947466b394e6d9ba0c493a435a
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-02-03 13:17:25 -0300

    Alterei o tamanho do button
    Coloquei pra corresponder ao tamanho da vh, pra ficar mais proporcional no mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   > svg {
-    width: 1%;
-    height: 1%;
+    width: 3vh;
+    height: 3vh;
     margin-top: 1%;
     margin-left: 1%;
     cursor: pointer;
